f81452b972c8f1a2e7e88b3f4588742eeb6ffdde
sankum136 <sanjaik316@gmail.com>
2026-05-24 19:53:26 +0530

    Fixed static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,4 +1,5 @@
 import os
+
 """
 Django settings for myproject project.
 
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,9 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     
 # Razorpay Settings
 RAZORPAY_KEY_ID = 'rzp_test_YOUR_KEY_ID'
